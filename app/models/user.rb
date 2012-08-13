53ddd88f6f58138fd5a2d62846fa51b9ee989d5e
Evan Hahn <me@evanhahn.com>
2012-08-12 21:52:51 -1000

    Users! Thanks, Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :lat, :lon
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :lat, :lon, :email, :password, :password_confirmation, :remember_me
   validates_presence_of :name
   validates_presence_of :lat
   validates_presence_of :lon
