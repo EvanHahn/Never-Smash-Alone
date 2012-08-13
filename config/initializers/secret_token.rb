0856195db0bd18371f984b53a41a3380fd7b4692
Evan Hahn <me@evanhahn.com>
2012-08-12 21:14:19 -1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nsa::Application.config.secret_token = '0ad99105f26af338d81e89baeff77246d2a718668aec7de7ca9e322431d1b0212970b30795490b2d3bf577513eab80aab4c7d2f89a3a1bc04292712dd10a82d0'
