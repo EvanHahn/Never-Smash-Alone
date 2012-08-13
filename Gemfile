b7f0d2ec3fb3f748fa54bc74395987bf07fdff26
Evan Hahn <me@evanhahn.com>
2012-08-13 13:26:27 -1000

    First spec\!

--- Gemfile
+++ Gemfile
@@ -20,11 +20,6 @@ group :test, :development do
   gem 'hirb'
 end
 
-group :test do
-  gem 'factory_girl_rails'
-  gem 'capybara'
-end
-
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe 'User' do
+
+  it 'should have the right accessible attributes' do
+    User.accessible_attributes.to_a.reject(&:blank?).should =~ %w(name latitude longitude email password password_confirmation remember_me)
+  end
+
 end
