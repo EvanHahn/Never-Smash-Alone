160f7ce523c82b4141f846f4d509788f6d76972d
Evan Hahn <me@evanhahn.com>
2012-08-13 13:58:29 -1000

    A very unfinished Message spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+#   before :each do
+#     pass = 'abc123'
+#     @roy = User.create! :name => 'roy', :password => pass, :password_confirmation => pass, :email => 'roy@example.com', :latitude => 12, :longitude => -12
+#     @captain_falcon = User.create! :name => 'cfalcon', :password => pass, :password_confirmation => pass, :email => 'cfalcon@example.com', :latitude => 12, :longitude => -12
+#   end
+
+#   describe 'sender' do
+
+#     it 'should return the first sender participant' do
+#       message = Message.new :body => 'Show me your moves!'
+#       message.users.add @roy
+#       message.users.add @captain_falcon
+#     end
+
+#   end
+
 end
