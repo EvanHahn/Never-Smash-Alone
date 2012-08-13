ca051abb4d3f9e8928f1819982d800b8cb542b80
Evan Hahn <me@evanhahn.com>
2012-08-12 21:22:51 -1000

    Minor: reorder some attr_accessibles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :lat, :lon, :name
+  attr_accessible :name, :lat, :lon
   validates_presence_of :name
   validates_presence_of :lat
   validates_presence_of :lon
